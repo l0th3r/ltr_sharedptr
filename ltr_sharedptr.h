0552a0a56ddbd74d94d00a01a7af57f48d57eac2
Cosmo <58365407+l0th3r@users.noreply.github.com>
2022-05-25 14:20:36 +0100

    Done MakeShared, moved to a static function

--- ltr_sharedptr.h
+++ ltr_sharedptr.h
@@ -3,21 +3,25 @@
 namespace ltr
 {
 	template<class T>
-	class MakeSharedPtr;
-
-	template<class T>
 	class SharedPtr
 	{
-	public:
-		SharedPtr(const T* ptr = nullptr);
-		SharedPtr(const SharedPtr& sharedptr);
+	private:
+		SharedPtr(T* ptr = nullptr);
 
+	public:
 		unsigned int getRefCount();
 
 		T& operator*();
 		T& operator->();
 
-		SharedPtr<T>& operator=(const MakeSharedPtr<T>&);
+		SharedPtr<T>& operator=(const SharedPtr<T>& msptr);
+		
+		template<class... Args>
+		static inline SharedPtr<T> Make(Args... args)
+		{
+			auto sptr = SharedPtr<T>(new T(args...));
+			return sptr;
+		}
 
 		~SharedPtr();
 
@@ -27,13 +31,66 @@ namespace ltr
 
 	};
 
-	template<class T>
-	class MakeSharedPtr
-	{
-	private:
-		SharedPtr<T>* m_sharedPtr;
+#pragma region Constructors and Destructors
 
-	public:
-		MakeSharedPtr();
-	};
+	template<class T>
+	SharedPtr<T>::SharedPtr(T* ptr) : m_ptr(ptr), m_counter(new unsigned int(0))
+	{
+		if (ptr != nullptr)
+			(*m_counter)++;
+	}
+
+	template<class T>
+	SharedPtr<T>::~SharedPtr()
+	{
+		(*m_counter)--;
+
+		if (this->getRefCount() <= 0)
+		{
+			delete m_ptr;
+			delete m_counter;
+		}
+	}
+
+#pragma endregion
+
+#pragma region Methods
+
+	template<class T>
+	unsigned int SharedPtr<T>::getRefCount()
+	{
+		return *m_counter;
+	}
+
+#pragma endregion
+
+#pragma region Operators
+
+	template<class T>
+	T& SharedPtr<T>::operator*()
+	{
+		return *m_ptr;
+	}
+
+	template<class T>
+	T& SharedPtr<T>::operator->()
+	{
+		return m_ptr;
+	}
+
+	template<class T>
+	SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& sptr)
+	{
+		this->m_ptr = sptr.m_ptr;
+		this->m_counter = sptr.m_counter;
+
+		return *this;
+	}
+
+
+#pragma endregion
+
+#pragma region MakeSharedPtr
+
+#pragma endregion
 }
--- main.cpp
+++ main.cpp
@@ -2,20 +2,20 @@
 
 #include "ltr_sharedptr.h"
 
+using namespace ltr;
+
 class Data
 {
 public:
-	Data(int value = 30)
-	{
-		m_value = value;
-	}
-
+	Data(int id, int value = 30) : m_id(id), m_value(value) {}
+	
+	int m_id;
 	int m_value;
 };
 
 int main(void)
 {
-	ltr::SharedPtr<Data> ptrData = ltr::MakeSharedPtr<Data>();
+	ltr::SharedPtr<Data> d = ltr::SharedPtr<Data>::Make(10, 50);
 
 	std::cout << "Hello World !" << std::endl;
 	std::cin.get();
